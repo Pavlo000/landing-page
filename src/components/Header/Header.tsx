906677b50f583097d7bb40222bdab0ccf14f810c
Pasha Moroz <pmoroz2004@gmail.com>
2023-06-29 22:22:15 +0300

    init basic components

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import { Nav } from '../Nav';
+import './Header.scss';
+
+export const Header: React.FC = () => {
+  return (
+    <header className="Header">
+      header
+      <Nav />
+    </header>
+  );
+}
---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,9 @@
+import './Nav.scss';
+
+export const Nav: React.FC = () => {
+  return (
+    <nav>
+      nav
+    </nav>
+  );
+}
